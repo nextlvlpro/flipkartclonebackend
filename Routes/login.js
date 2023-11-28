615c1c06b69b40e220555d5990fcb2bd97f5a692
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-11-28 22:39:44 +0530

    Update login.js

--- Routes/login.js
+++ Routes/login.js
@@ -27,7 +27,7 @@ loginRoute.post('/login', async(req,res) => {
 })
 
 loginRoute.post('/logout', (req,res) => {
-   res.cookie('token','').json('done')
+   res.cookie('token','', {sameSite:'none', secure:true}).json('done')
 })
 
 module.exports = loginRoute
